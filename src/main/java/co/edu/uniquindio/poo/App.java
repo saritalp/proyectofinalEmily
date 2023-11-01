fe2d49019fafaf255b2bf2ea2d3dedb3315577a5
saritalp <saritalondonop@gmail.com>
2023-11-01 16:32:02 -0500

    creado

--- src/main/java/co/edu/uniquindio/poo/App.java
+++ src/main/java/co/edu/uniquindio/poo/App.java
@@ -8,6 +8,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-    private final String clase;
+    //private final String clase;
     /*hola chicos esta esS una prueba  hi    holiiiiii*/
 }
